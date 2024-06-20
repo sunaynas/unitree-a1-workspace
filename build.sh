9d1f1c17f0b654b6c4e886efffe3fc0302ba3d25
guest <me@sunayna.com>
2024-06-20 11:10:30 +0100

    build.sh works i think

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ export GAZEBO_PLUGIN_PATH=~/catkin_ws/devel/lib:${GAZEBO_PLUGIN_PATH}
 export LD_LIBRARY_PATH=~/catkin_ws/devel/lib:${LD_LIBRARY_PATH}
 # 3_1, 3_2
 export UNITREE_SDK_VERSION=3_2
-export UNITREE_LEGGED_SDK_PATH=~/unitree_legged_sdk_v3.2
+export UNITREE_LEGGED_SDK_PATH=~/catkin_ws/src/unitree_legged_sdk
 # amd64, arm32, arm64
 export UNITREE_PLATFORM="arm64"
 cd ~/catkin_ws
