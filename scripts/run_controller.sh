c6116902bd8ab436c8c0d06ce6c83b243d424557
guest <me@sunayna.com>
2024-06-28 11:55:25 +0100

    run_controller script works

--- scripts/run_controller.sh
+++ scripts/run_controller.sh
@@ -1,12 +1,46 @@
+
 cd ~/catkin_ws
 ./build.sh &
+build_pid=$!
+wait $build_pid
+
+echo ""
+echo "Build complete"
+
+while true; do
+	echo ""
+	echo "Please enter your sudo password:"
+	read -s password
+	echo "$password" | sudo -S -v &>/dev/null
+	if [ $? -eq 0 ]; then
+		break
+	else
+		echo "Incorrect password."
+	fi
+done 
+
+echo ""
+echo "Starting roscore..."
 gnome-terminal -- bash -c 'roscore' &
+sleep 5
 
-echo "Please enter your sudo password:"
-read -s password
+echo ""
+echo "Launching the launch file..."
+gnome-terminal -- bash -c "echo $password | sudo -S bash -c 'source devel/setup.bash; roslaunch unitree_legged_real real.launch'" & 
+sleep 5
 
-echo "$password" | sudo -S bash -c 'source devel/setup.bash' &
+echo ""
+echo "Running the controller..."
+gnome-terminal -- bash -c 'rosrun unitree_legged_real controller'&
+sleep 5
 
-echo "$password" | sudo -S bash -c 'roslaunch unitree_legged_real real.launch' &
-
-gnome-terminal -- bash -c 'rosrun unitree_legged_real controller' 
+echo ""
+echo "DONE!"
+echo ""
+echo "To manually send requests you can run:"
+echo 'rosservice call /controller_node/control_a1 "mode: 1'
+echo '	forwardSpeed: 0.0'
+echo '	sideSpeed: 0.0'
+echo '	rotateSpeed: 0.0"'
+echo "but replace the numbers with your desired values. "
+echo "Mode 1 is stationary sports mode, and mode 2 is walking sports mode."
